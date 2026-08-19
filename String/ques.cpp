bc6217da0c73a9a6b3c370f50d985f001e26b4ee
fauzia khan <FAUZIAKHAN7266@gmail.com>
2026-08-19 10:15:20 +0530

    string ques done

---
+++ String/ques.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s,s2;
+    s="\\n";
+    s2="\\0";
+    cout<<s<<endl<<s2;
+}
---
+++ String/userinput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter the string:";
+    getline(cin,s);
+    cout<<s<<endl;
+    int n=s.size();
+    cout<<n;
+}
